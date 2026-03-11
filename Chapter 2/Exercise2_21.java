d07c355d632f1013afa8087493d5b8e144aa613f
trustgod1991 <trustgodanyah5@gmail.com>
2026-03-11 13:08:00 +0100

    Chapter 2

---
+++ Chapter 2/Exercise2_21.java
@@ -0,0 +1,9 @@
+public class Exercise2_21{
+	public static void main(String[] args){
+		System.out.print("*");
+		System.out.print("***");
+		System.out.print("*****");
+		System.out.print("****");
+		System.out.println("**");
+	}
+}
